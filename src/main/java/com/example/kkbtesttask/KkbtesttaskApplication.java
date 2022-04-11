8017aa16b588a0e6b2da8cfc913af2da06f2416f
boomzin windows work <boomzin@gmail.com>
2022-04-11 13:12:31 +0300

    Initial commit

---
+++ src/main/java/com/example/kkbtesttask/KkbtesttaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.kkbtesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkbtesttaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkbtesttaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kkbtesttask/KkbtesttaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kkbtesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkbtesttaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
