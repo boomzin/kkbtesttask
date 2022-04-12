1a4f213eddaf66b724edd75171a1f776702fba74
boomzin windows work <boomzin@gmail.com>
2022-04-12 14:33:49 +0300

    init and populate db

---
+++ src/main/resources/database/populateDB.sql
@@ -0,0 +1,9 @@
+INSERT INTO task (NAME, DESCRIPTION, STATUS)
+VALUES ('Make a task list', 'main task', 'DONE'),
+       ('Check off the first thing', 'check that the task list is working', 'DONE'),
+       ('Check the status of task list', 'realise you have already completed 2 thing on the list', 'DONE'),
+       ('Reward yourself with applause', 'clap clap', 'DONE'),
+       ('Make a task tracker', 'add some functional', 'OPEN'),
+       ('Pass the test task', 'finish task and send result', 'OPEN');
+
+
