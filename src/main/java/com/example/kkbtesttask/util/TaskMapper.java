50c954e6a5defcaaee4ec5b507f9f60b59bc972b
boomzin windows work <boomzin@gmail.com>
2022-04-12 17:19:11 +0300

    add mapstruct and mapper

---
+++ src/main/java/com/example/kkbtesttask/util/TaskMapper.java
@@ -0,0 +1,16 @@
+package com.example.kkbtesttask.util;
+
+import com.example.kkbtesttask.dto.TaskDto;
+import com.example.kkbtesttask.model.Task;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public interface TaskMapper {
+
+    TaskDto toTaskDto(Task task);
+
+    List<TaskDto> toListTaskDto(List<Task> tasks);
+}
